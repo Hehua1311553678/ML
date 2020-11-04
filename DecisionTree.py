0dedb09e0bd23fa45307d2ac7914c96fd7d21b6c
Hehua <1311553678@qq.com>
2020-11-04 23:08:09 +0800

    ID3和C45

--- DecisionTree.py
+++ DecisionTree.py
@@ -42,7 +42,7 @@ def InformatinGain(feature, tr_X, tr_Y):
     return H - H_feature
 
 def InformationGainRatio(feature, tr_X, tr_Y):
-    '''计算feature带来的信息增益
+    '''计算feature带来的信息增益比
     feature: 特征索引（如：0,1,2,...）；
     tr_X: 训练数据集的X；
     tr_Y: 训练数据集的Y；
@@ -56,3 +56,96 @@ def InformationGainRatio(feature, tr_X, tr_Y):
     # 3. 计算信息增益比；
     gR = g/H_feature
     return gR
+
+def get_featrue_with_max_IG(features, X, Y):
+    '''
+    计算信息增益最大的特征
+    @param features:特征索引（如：[0,1,2,...]）；
+    @param X:训练数据集的X；
+    @param Y:训练数据集的Y；
+    @return:信息增益最大的特征feature和对应的信息增益
+    '''
+    max_IG, max_feature = 0, None
+    for feature in features:
+        IG = InformatinGain(feature, X, Y)
+        if IG > max_IG:
+            max_IG, max_feature = IG, feature
+    return max_feature, max_IG
+
+def get_featrue_with_max_IGR(features, X, Y):
+    '''
+    计算信息增益比最大的特征
+    @param features:特征索引（如：[0,1,2,...]）；
+    @param X:训练数据集的X；
+    @param Y:训练数据集的Y；
+    @return:信息增益比最大的特征feature和对应的信息增益比
+    '''
+    max_IGR, max_feature = 0, None
+    for feature in features:
+        IGR = InformationGainRatio(feature, X, Y)
+        if IGR > max_IGR:
+            max_IGR, max_feature = IGR, feature
+    return max_feature, max_IGR
+
+def ID3(X, Y, features, epsilon):
+    '''
+    ID3算法（生成决策树）
+    @param X:训练数据集的X；
+    @param Y:训练数据集的Y；
+    @param features:特征索引（如：[0,1,2,...]）；
+    @param epsilon:信息增益阈值；
+    @return:决策树T。
+    '''
+    # 1. 若Y中只有一类Ck，则T为单节点树，类别为Ck，返回T;
+    if len(list(set(Y))) == 1:
+        return Y[0]
+    # 2. 若features为空，则T为单节点树，类别为Y中实例数最大的类别，返回T;
+    if len(features) == 0:
+        return max(Y, Y.count)
+    else:
+        # 3. 否则计算信息增益最大的特征feature;
+        feature, IG = get_featrue_with_max_IG(features, X, Y)
+        # 4. 若feature的信息增益小于阈值epsilon，则T为单节点树，类别为Y中实例数最大的类别，返回T;
+        if IG < epsilon:
+            return max(Y, key=Y.count)
+        else:
+            # 5. 否则，根据feature所有取值划分数据集，构建子节点，返回T;
+            dict_node = {}
+            values = set(X[feature])
+            for value in values:
+                index = [True if x[feature] == value else False for x in X]
+                # 6. 对第i个子节点，以子数据集和features-featrue，递归调用1-5，得到子树Ti，返回Ti.
+                dict_node['value'] = ID3(X[index], Y[index], features.remove(feature), epsilon)
+            return {feature: dict_node}
+
+def C45(X, Y, features, epsilon):
+    '''
+    C4.5算法（生成决策树）
+    @param X:训练数据集的X；
+    @param Y:训练数据集的Y；
+    @param features:特征索引（如：[0,1,2,...]）；
+    @param epsilon:信息增益阈值；
+    @return:决策树T。
+    '''
+    # 1. 若Y中只有一类Ck，则T为单节点树，类别为Ck，返回T;
+    if len(list(set(Y))) == 1:
+        return Y[0]
+    # 2. 若features为空，则T为单节点树，类别为Y中实例数最大的类别，返回T;
+    if len(features) == 0:
+        return max(Y, Y.count)
+    else:
+        # 3. 否则计算信息增益比最大的特征feature;
+        feature, IGR = get_featrue_with_max_IGR(features, X, Y)
+        # 4. 若feature的信息增益比小于阈值epsilon，则T为单节点树，类别为Y中实例数最大的类别，返回T;
+        if IGR < epsilon:
+            return max(Y, key=Y.count)
+        else:
+            # 5. 否则，根据feature所有取值划分数据集，构建子节点，返回T;
+            dict_node = {}
+            values = set(X[feature])
+            for value in values:
+                index = [True if x[feature] == value else False for x in X]
+                # 6. 对第i个子节点，以子数据集和features-featrue，递归调用1-5，得到子树Ti，返回Ti.
+                dict_node['value'] = ID3(X[index], Y[index], features.remove(feature), epsilon)
+            return {feature: dict_node}
+
