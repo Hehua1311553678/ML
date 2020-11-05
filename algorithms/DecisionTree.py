a9742a20d3511c36be4ee73b2c9683a9945f641c
Hehua <1311553678@qq.com>
2020-11-05 21:47:25 +0800

    最小二次回归树

--- algorithms/DecisionTree.py
+++ algorithms/DecisionTree.py
@@ -149,3 +149,75 @@ def C45(X, Y, features, epsilon):
                 dict_node['value'] = ID3(X[index], Y[index], features.remove(feature), epsilon)
             return {feature: dict_node}
 
+def classify(T, te_x):
+    '''
+    根据决策树T，对te_x进行分类
+    @param T:决策树;
+    @param te_x:测试示例；
+    @return:分类结果。
+    '''
+    # 1. 根据第一个特征取值找子节点；
+    first_feature = int(list(T.keys())[0])
+    first_values_dict = T[first_feature]
+    first_value = te_x[first_feature]
+    second_dict = first_values_dict[first_value]
+    # 2. 若子节点是树（字典）则递归查询子树；
+    if type(second_dict) == dict:
+        return classify(second_dict, te_x)
+    else:
+        # 3. 否则返回当前类别。
+        return second_dict
+
+def calculate_loss(X, Y, j, s):
+    '''
+    根据j, s计算式（5.21）的值（loss）
+    @param X:训练数据集的X；
+    @param Y:训练数据集的Y；
+    @param j:切分变量；
+    @param s:切分点；
+    @return:loss值。
+    '''
+    index1 = np.array([True if x[j] <= s else False for x in X])
+    index2 = ~index1
+    R1, Y1 = list(np.array(X)[index1]), np.array(Y)[index1]
+    R2, Y2 = list(np.array(X)[index2]), np.array(Y)[index2]
+
+    var1, var2 = Y1.var(), Y2.var()
+    loss = len(Y1) * var1 + len(Y2) * var2
+    return loss, R1, R2, list(Y1), list(Y2), Y1.mean(), Y2.mean()
+
+def Least_Squares_Regression_Tree(X, Y, times, total_times):
+    '''
+    最小二乘回归树生成算法
+    @param X:训练数据集的X；
+    @param Y:训练数据集的Y；
+    @param times:划分次数;
+    @param total_times:划分总次数;
+    @return:回归树。
+    '''
+    if len(X) == 0: return 0
+    if len(X) == 1: return Y[0]
+    if times == total_times:
+        return np.array(Y).mean()
+    # 1. 选择最优切分变量j与切分点s；
+    features = np.arange(len(X[0]))
+    min_loss, best_j, best_s = -1, -1, -1
+    for j in features:
+        values_j = set([x[j] for x in X])
+        for s in values_j:
+            loss, _, _, _, _, _, _ = calculate_loss(X, Y, j, s)
+            if (min_loss == -1) or (loss < min_loss):
+                min_loss, best_j, best_s = loss, j, s
+    # 2. 用选定的对(j, s)划分区域并决定相应的输出值；
+    _, R1, R2, Y1, Y2, c1, c2 = calculate_loss(X, Y, best_j, best_s)
+    # 3. 继续对两个子区域调用步骤1,2，直至满足停止条件；
+    T1 = Least_Squares_Regression_Tree(R1, Y1, times + 1, total_times)
+    T2 = Least_Squares_Regression_Tree(R2, Y2, times + 1, total_times)
+    # 4. 将输入空间划分为M个区域R1, R2, ..., RM生成决策树。
+    return {j: {s: T1, s+1: T2}}
+
+if __name__ == '__main__':
+    X = [[1, 1], [1, 1], [1, 0], [0, 1], [0, 1]]
+    Y = ['yes', 'yes', 'no', 'no', 'no']
+    T = Least_Squares_Regression_Tree(X, Y, 0, 2)
+    print(T)
